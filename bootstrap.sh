f04ef2ee3e3229c68d4fd20baadd775e3a577c93
Richard Ortega <richard.j.ortega2@boeing.com>
2015-04-22 13:42:42 -0700

    update ruby to 2.2.2

--- bootstrap.sh
+++ bootstrap.sh
@@ -48,9 +48,9 @@ echo adding docker service to systemctl
 systemctl enable docker.service >/dev/null 2>&1
 systemctl start docker.service >/dev/null 2>&1
 
-echo installing Ruby 2.2.1 via RVM
+echo installing Ruby 2.2.2 via RVM
 su - vagrant -c 'gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3' >/dev/null 2>&1
-su - vagrant -c 'curl -sSL https://get.rvm.io | bash -s stable --ruby=2.2.1' >/dev/null 2>&1
+su - vagrant -c 'curl -sSL https://get.rvm.io | bash -s stable --ruby=2.2.2' >/dev/null 2>&1
 su - vagrant -c 'rvm rvmrc warning ignore allGemfiles' >/dev/null 2>&1
 su - vagrant -c 'source /home/vagrant/.profile'
 su - vagrant -c 'source /home/vagrant/.rvm/scripts/rvm'
